273fdc05b80f47bbad8e9787053419ddb73749f0
ArshiyaMilady <ArshiyaMilady@gmail.com>
2021-09-14 22:36:03 +0430

    1400-06-23-a

---
+++ DataAccessLibrary/EntityModels/ContactDBContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactJs_Redux_01.EntityModels
+{
+    public partial class ContactDBContext : DbContext
+    {
+        public ContactDBContext()
+        {
+        }
+
+        public ContactDBContext(DbContextOptions<ContactDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contacts> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=ContactDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Contacts>(entity =>
+            {
+                entity.HasKey(e => e.ContactId)
+                    .HasName("PK_Contact");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.Property(e => e.Phone).HasMaxLength(50);
+            });
+        }
+    }
+}
